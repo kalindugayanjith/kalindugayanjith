0f8f4bbc38a598b0a55325b650f0b1c653bc9efc
Kalindu Gayanjith <kalindugayanjith@gmail.com>
2025-11-04 18:21:23 +0530

    vanta js

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,11 +1,46 @@
-import Link from "next/link";
+"use client";
+
+import { useEffect } from "react";
+import Script from "next/script";
 
 export default function HomePage() {
+useEffect(() => {
+   const initVanta = () => {
+      if (window.VANTA && !window.vantaEffect) {
+        window.vantaEffect = window.VANTA.WAVES({
+          el: "#vanta-bg",
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.0,
+          minWidth: 200.0,
+          scale: 1.0,
+          scaleMobile: 1.0,
+          color: "#000", 
+          shininess: 20.0,
+          waveHeight: 4.0,
+          waveSpeed: 0.8,
+          backgroundColor: "#000", 
+        });
+      }
+    };
+
+    const interval = setInterval(() => {
+      if (window.VANTA) {
+        clearInterval(interval);
+        initVanta();
+      }
+    }, 300);
+
+    return () => {
+      if (window.vantaEffect) window.vantaEffect.destroy();
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
-    <div>
-        <Link href="/about">Learn more about us</Link>
-      <h1>Welcome to My Portfolio</h1>
-      <p>This is the home page content.</p>
-    </div>
+    <>
+      <div id="vanta-bg" className="home-page"></div>
+    </>
   );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,9 @@ export default function MainLayout({ children }) {
       <body cz-shortcut-listen="true">
         <AppLayout>{children}</AppLayout>
 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.waves.min.js"></script>
+
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
@@ -24,4 +27,4 @@ export default function MainLayout({ children }) {
       </body>
     </html>
   );
-} 
+}
